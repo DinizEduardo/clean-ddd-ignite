072c73c2b31a8fb54e0c82cd43be18e064d64cb2
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-07-26 20:43:05 -0300

    feat: mapeando novas propriedades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface AnswerProps {
   content: string
-  authorId: string
-  questionId: string
+  authorId: UniqueEntityID
+  questionId: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
@@ -11,5 +14,5 @@ export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content
   }
-  
+
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
   title: string
   content: string
-  authorId: string
+  authorId: UniqueEntityID
   slug: Slug
+  bestAnswerId?: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
