24cc6316fca891a40af3d6c075cff48b88731dd6
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-07-28 09:03:21 -0300

    test: gerando dados ficticios com fakerjs

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,14 +3,21 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'Example question',
-    authorId: new UniqueEntityID(),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      authorId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
