9fb13427fa3cc8e4b3bcb92d2be9f4f9b7d10348
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-01 14:18:15 -0300

    feat: use-case listar perguntas recentes

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionRepositories } from 'test/repositories/in-memory-question-repositories'
+import { FetchRecentQuestionsUseCase } from './fetch-recent-questions'
+
+let inMemoryQuestionsRepository: InMemoryQuestionRepositories
+let sut: FetchRecentQuestionsUseCase
+
+describe('Fetch recent questions', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionRepositories()
+    sut = new FetchRecentQuestionsUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('Should be able to get a fetch recent questions', async () => {
+    await inMemoryQuestionsRepository.create(
+      makeQuestion({ createdAt: new Date(2022, 0, 20) }),
+    )
+    await inMemoryQuestionsRepository.create(
+      makeQuestion({ createdAt: new Date(2022, 0, 18) }),
+    )
+    await inMemoryQuestionsRepository.create(
+      makeQuestion({ createdAt: new Date(2022, 0, 23) }),
+    )
+
+    const { questions } = await sut.execute({
+      page: 1,
+    })
+
+    expect(questions).toEqual([
+      expect.objectContaining({ createdAt: new Date(2022, 0, 23) }),
+      expect.objectContaining({ createdAt: new Date(2022, 0, 20) }),
+      expect.objectContaining({ createdAt: new Date(2022, 0, 18) }),
+    ])
+  })
+
+  it('Should be able to get a fetch paginated recent questions', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionsRepository.create(makeQuestion())
+    }
+    const { questions } = await sut.execute({
+      page: 2,
+    })
+
+    expect(questions).toHaveLength(2)
+  })
+})
