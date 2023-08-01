4698dd7fe739b953d38ce33ebc67808d32cc76e9
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-01 15:31:28 -0300

    feat: criação das entidades de comentarios

---
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -0,0 +1,57 @@
+import { Optional } from '@/core/@types/optional'
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+export interface AnswerCommentProps {
+  authorId: UniqueEntityID
+  answerId: UniqueEntityID
+  content: string
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class AnswerComment extends Entity<AnswerCommentProps> {
+  get content() {
+    return this.props.content
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  get answerId() {
+    return this.props.answerId
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  static create(
+    props: Optional<AnswerCommentProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const answerComment = new AnswerComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return answerComment
+  }
+}
