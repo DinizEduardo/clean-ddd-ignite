b4c1b9f3cd58b50df69bd3b297b7e4baf782d08e
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-02 11:55:59 -0300

    feat: use-case listar comentarios da pergunta

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeQuestionComment } from 'test/factories/make-question-comment'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { FetchQuestionCommentsUseCase } from './fetch-question-comments'
+
+let inMemoryQuestionCommentRepository: InMemoryQuestionCommentsRepository
+let sut: FetchQuestionCommentsUseCase
+
+describe('Fetch question comments', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentRepository = new InMemoryQuestionCommentsRepository()
+    sut = new FetchQuestionCommentsUseCase(inMemoryQuestionCommentRepository)
+  })
+
+  it('Should be able to get a fetch questions questioncomment', async () => {
+    await inMemoryQuestionCommentRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+    await inMemoryQuestionCommentRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+    await inMemoryQuestionCommentRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+
+    const { questionComments } = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    })
+
+    expect(questionComments).toHaveLength(3)
+  })
+
+  it('Should be able to get a fetch paginated question questioncomment', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionCommentRepository.create(
+        makeQuestionComment({
+          questionId: new UniqueEntityID('question-1'),
+        }),
+      )
+    }
+    const { questionComments } = await sut.execute({
+      questionId: 'question-1',
+      page: 2,
+    })
+
+    expect(questionComments).toHaveLength(2)
+  })
+})
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
@@ -22,6 +23,17 @@ export class InMemoryQuestionCommentsRepository
     }
 
     return questionComment
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<QuestionComment[]> {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
   }
 
   async create(questionComment: QuestionComment): Promise<void> {
