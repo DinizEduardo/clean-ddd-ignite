cf9149009217bf71f11f2252ecc51c90035429b1
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-01 21:16:55 -0300

    feat: use-case comentar na resposta

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments'
+import { InMemoryAnswerRepositories } from 'test/repositories/in-memory-answers-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let inMemoryAnswerRepository: InMemoryAnswerRepositories
+let sut: CommentOnAnswerUseCase
+
+describe('Comment on answer', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    inMemoryAnswerRepository = new InMemoryAnswerRepositories()
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswerRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswerRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Comentario teste',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      'Comentario teste',
+    )
+  })
+
+  it('should not be able to comment on an unexistent answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswerRepository.create(answer)
+
+    await expect(async () => {
+      await sut.execute({
+        answerId: 'answer-1',
+        authorId: answer.authorId.toString(),
+        content: 'Comentario teste',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
