fbf88c652185e1a4c52daae0b1ebdac5f8cda90e
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-01 21:29:14 -0300

    feat: use-case deletar comentario da pergunta

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -6,6 +6,24 @@ export class InMemoryQuestionCommentsRepository
 {
   public items: QuestionComment[] = []
 
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
+  }
+
+  async findById(id: string): Promise<QuestionComment | null> {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
   async create(questionComment: QuestionComment): Promise<void> {
     this.items.push(questionComment)
   }
